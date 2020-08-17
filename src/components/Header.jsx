b3f8de67ff3ace9f1879040187848e521aac3f63
David Isakov <davamarkovich1217@gmail.com>
2020-08-17 15:47:40 +0300

    Way-of-Samurai 13 lesson

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/DC_Comics_logo.png/600px-DC_Comics_logo.png'></img>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img src="https://images3.alphacoders.com/106/1069102.jpg" />
+            </div>
+            <div>ava + description</div>
+            <div>My posts</div>
+            <div>New posts</div>
+            <div>
+                <div>Post 1</div>
+                <div>Post 2</div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
